b736fb57881b81d6794e0432609ac40cbe549d4c
Moussa Noe <moussa.noe@justice.fr>
2024-10-01 16:32:39 +0200

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FireworkComponent } from './firework/firework.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FireworkComponent,
+    NavigationBarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.css'],
+})
+export class NavigationBarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
