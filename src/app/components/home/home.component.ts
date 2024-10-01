5ecc2b45858667d6235802f0827895c063ae4b3f
Moussa Noe <moussa.noe@justice.fr>
2024-10-01 13:54:49 +0200

    e

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,28 +8,25 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   showFirework = false;
   fireworkTimeout: any;
-  isHovered = false; // Pour suivre l'état du survol
+  isHovered = false;
 
   onMouseEnter() {
-    // Démarre l'animation des feux d'artifice si elle n'est pas déjà affichée
     if (!this.showFirework) {
-      this.showFirework = true; // Afficher les feux d'artifice lors du survol
-      this.isHovered = true; // Activer l'animation
+      this.showFirework = true;
+      this.isHovered = true;
 
-      // Si un timeout existe, le supprimer
+
       if (this.fireworkTimeout) {
         clearTimeout(this.fireworkTimeout);
       }
-      // Cacher les feux d'artifice après 4 secondes
+
       this.fireworkTimeout = setTimeout(() => {
         this.showFirework = false;
-        this.isHovered = false; // Désactiver l'animation après 4 secondes
+        this.isHovered = false;
       }, 1700);
     }
   }
 
   onMouseLeave() {
-    // Lorsque la souris quitte l'élément, on ne fait rien ici
-    // L'animation et les feux d'artifice sont déjà gérés par le timeout.
   }
 }
