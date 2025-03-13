3a94b3e08ec383f56cdeadb859216e8b8afdf370
noe moussa <noe.mo@justice.fr>
2025-03-13 08:48:47 +0100

    scroll up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test';
+  showScrollButton = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.showScrollButton = window.scrollY > 200;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
